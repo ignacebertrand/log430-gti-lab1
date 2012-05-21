c77c5ad230c6214e17bb184b79605110ed190d89
alexandre.fleury.lecompte <alexandre.fleury.lecompte@gmail.com>
2012-05-21 21:39:31 +0000

    Finished building modificationA.

--- log430-gti-lab1/modificationA/SeverityFilter.java
+++ log430-gti-lab1/modificationA/SeverityFilter.java
@@ -3,169 +3,159 @@ package modificationA;
 import java.io.PipedReader;
 import java.io.PipedWriter;
 
-/**************************************************************************************
- ** Class name: SeverityFilter
- ** Original author: A.J. Lattanze, CMU
- ** Date: 12/3/99
- ** Version 1.0
- **
- ** Adapted by R. Champagne, Ecole de technologie superieure
- ** 2002-May-08, 2012-Jan-13, 2012-May-10
- **
- ***************************************************************************************
- ** Purpose: Assignment 1 for LOG430, Architectures logicielle. This
- ** assignment is designed to illustrate a pipe and filter architecture.  For the 
- ** instructions, refer to the assignment write-up.
- **
- ** Abstract: This class is intended to be a filter that will key on a particular severity
- **	     provided at instantiation.  Note that the stream has to be buffered so that
- **      it can be checked to see if the specified severity appears on the stream.
- **      If this string appears in the stream from Main, to the output stream.
- ** 
- ** Pseudo Code:
- **
- ** 	connect to input pipe
- ** 	connect to output pipe
- **
- **	while not end of line
- **
- **		read input pipe
- **
- **		if specified severity appears on line of text
- **			write line of text to output pipe
- **			flush pipe
- **		end if
- **
- **	end while
- **	close pipes
- **
- ** Running the program
- **
- ** 	See Main.java
- **
- ** Modification Log
- **************************************************************************************
- **
- **************************************************************************************/
-
+/**
+ * ************************************************************************************
+ ** Class name: SeverityFilter * Original author: A.J. Lattanze, CMU * Date:
+ * 12/3/99 * Version 1.0 * * Adapted by R. Champagne, Ecole de technologie
+ * superieure * 2002-May-08, 2012-Jan-13, 2012-May-10 *
+ * **************************************************************************************
+ * * Purpose: Assignment 1 for LOG430, Architectures logicielle. This *
+ * assignment is designed to illustrate a pipe and filter architecture. For the
+ * * instructions, refer to the assignment write-up. * * Abstract: This class is
+ * intended to be a filter that will key on a particular severity *	provided at
+ * instantiation. Note that the stream has to be buffered so that * it can be
+ * checked to see if the specified severity appears on the stream. * If this
+ * string appears in the stream from Main, to the output stream. * * Pseudo
+ * Code: * * connect to input pipe * connect to output pipe * *	while not end of
+ * line * *	read input pipe * *	if specified severity appears on line of text *
+ * write line of text to output pipe *	flush pipe *	end if * *	end while *	close
+ * pipes * * Running the program * * See Main.java * * Modification Log
+ * *************************************************************************************
+ * *
+ *************************************************************************************
+ */
 public class SeverityFilter extends Thread {
 
-	// Declarations
+    // Declarations
+    boolean Done;
+    boolean exclude;
+    String severity;
+    PipedReader inputPipe = new PipedReader();
+    PipedWriter outputPipe = new PipedWriter();
 
-	boolean Done;
+    public SeverityFilter(String severity, boolean exclude, PipedWriter inputPipe,
+            PipedWriter outputPipe) {
 
-	String severity;
-	PipedReader inputPipe = new PipedReader();
-	PipedWriter outputPipe = new PipedWriter();
+        this.exclude = exclude;
+        this.severity = severity;
 
-	public SeverityFilter(String severity, PipedWriter inputPipe,
-			PipedWriter outputPipe) {
+        try {
 
-		this.severity = severity;
+            // Connect inputPipe to Main
 
-		try {
+            this.inputPipe.connect(inputPipe);
+            System.out.println("SeverityFilter " + severity
+                    + ":: connected to upstream filter.");
 
-			// Connect inputPipe to Main
+            // Connect outputPipe to Merge
 
-			this.inputPipe.connect(inputPipe);
-			System.out.println("SeverityFilter " + severity
-					+ ":: connected to upstream filter.");
+            this.outputPipe = outputPipe;
+            System.out.println("SeverityFilter " + severity
+                    + ":: connected to downstream filter.");
 
-			// Connect outputPipe to Merge
+        } catch (Exception Error) {
 
-			this.outputPipe = outputPipe;
-			System.out.println("SeverityFilter " + severity
-					+ ":: connected to downstream filter.");
+            System.out.println("SeverityFilter " + severity
+                    + ":: Error connecting to other filters.");
 
-		} catch (Exception Error) {
+        } // try/catch
 
-			System.out.println("SeverityFilter " + severity
-					+ ":: Error connecting to other filters.");
+    } // Constructor
 
-		} // try/catch
+    // This is the method that is called when the thread is started in
+    // Main
+    public void run() {
+        
+        String[] severities;
+        if (this.severity.indexOf(";") != -1) {
+            severities = this.severity.split(";");
+        } else {
+            severities = new String[1];
+            severities[0] = this.severity;
+        }
 
-	} // Constructor
+        // Declarations
 
-	// This is the method that is called when the thread is started in
-	// Main
-	public void run() {
+        char[] CharacterValue = new char[1];
+        // char array is required to turn char into a string
+        String LineOfText = "";
+        // string is required to look for the keyword
+        int IntegerCharacter; // the integer value read from the pipe
 
-		// Declarations
+        try {
 
-		char[] CharacterValue = new char[1];
-		// char array is required to turn char into a string
-		String LineOfText = "";
-		// string is required to look for the keyword
-		int IntegerCharacter; // the integer value read from the pipe
+            Done = false;
 
-		try {
+            while (!Done) {
 
-			Done = false;
+                IntegerCharacter = inputPipe.read();
+                CharacterValue[0] = (char) IntegerCharacter;
 
-			while (!Done) {
+                if (IntegerCharacter == -1) { // pipe is closed
 
-				IntegerCharacter = inputPipe.read();
-				CharacterValue[0] = (char) IntegerCharacter;
+                    Done = true;
 
-				if (IntegerCharacter == -1) { // pipe is closed
+                } else {
 
-					Done = true;
+                    if (IntegerCharacter == '\n') { // end of line
 
-				} else {
+                        System.out.println("SeverityFilter " + severity
+                                + ":: received: " + LineOfText + ".");
 
-					if (IntegerCharacter == '\n') { // end of line
+                        boolean isMatched = false;
+                        for (String sev : severities) {
+                            if (LineOfText.indexOf(sev) != -1) {
+                                isMatched = true;
+                                break;
+                            }
+                        }
+                        
+                        if ((this.exclude && !isMatched) | (!this.exclude && isMatched)) {
 
-						System.out.println("SeverityFilter " + severity
-								+ ":: received: " + LineOfText + ".");
+                            System.out.println("SeverityFilter "
+                                    + severity + ":: sending: "
+                                    + LineOfText + " to output pipe.");
+                            LineOfText += new String(CharacterValue);
+                            outputPipe.write(LineOfText, 0, LineOfText.length());
+                            outputPipe.flush();
 
-						if (LineOfText.indexOf(severity) != -1) {
+                        }
 
-							System.out.println("SeverityFilter "
-									+ severity + ":: sending: "
-									+ LineOfText + " to output pipe.");
-							LineOfText += new String(CharacterValue);
-							outputPipe
-									.write(LineOfText, 0, LineOfText.length());
-							outputPipe.flush();
+                        LineOfText = "";
 
-						} // if
+                    } else {
 
-						LineOfText = "";
+                        LineOfText += new String(CharacterValue);
 
-					} else {
+                    } // if //
 
-						LineOfText += new String(CharacterValue);
+                } // if
 
-					} // if //
+            } // while
 
-				} // if
+        } catch (Exception Error) {
 
-			} // while
+            System.out.println("SeverityFilter::" + severity
+                    + " Interrupted.");
 
-		} catch (Exception Error) {
+        } // try/catch
 
-			System.out.println("SeverityFilter::" + severity
-					+ " Interrupted.");
+        try {
 
-		} // try/catch
+            inputPipe.close();
+            System.out.println("SeverityFilter " + severity
+                    + ":: input pipe closed.");
 
-		try {
+            outputPipe.close();
+            System.out.println("SeverityFilter " + severity
+                    + ":: output pipe closed.");
 
-			inputPipe.close();
-			System.out.println("SeverityFilter " + severity
-					+ ":: input pipe closed.");
+        } catch (Exception Error) {
 
-			outputPipe.close();
-			System.out.println("SeverityFilter " + severity
-					+ ":: output pipe closed.");
+            System.out.println("SeverityFilter " + severity
+                    + ":: Error closing pipes.");
 
-		} catch (Exception Error) {
+        } // try/catch
 
-			System.out.println("SeverityFilter " + severity
-					+ ":: Error closing pipes.");
-
-		} // try/catch
-
-	} // run
-
+    } // run
 } // class
---
+++ log430-gti-lab1/modificationA/StatusSortFilter.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modificationA;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author Alexandre
+ */
+public class StatusSortFilter extends Thread {
+    
+    PipedReader _Source = null;
+    PipedWriter _InputPipe = null;
+    PipedWriter _OutputPipe = null;
+    
+    public StatusSortFilter(PipedWriter inputPipe, PipedWriter outputPipe) {
+        this._InputPipe = inputPipe;
+        this._OutputPipe = outputPipe;
+        
+        try {
+            this._Source = new PipedReader();
+            this._Source.connect(this._InputPipe);
+        } catch (Exception ex) {
+            System.out.println("StatusSortFilter: An error occurred during initialization.");
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public void run() {
+        ArrayList<String> lines = new ArrayList();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(this._Source);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            
+            this.closeReader(reader);
+        } catch (Exception ex) {
+            System.out.println("StatusSortFilter: An error occurred reading from the input pipe.");
+            System.out.println(ex.getMessage());
+            
+            if (reader != null) {
+                this.closeReader(reader);
+            }
+        }
+        
+        StatusComparator comparator = this.new StatusComparator();
+        Collections.sort(lines, comparator);
+        
+        try {
+            for (int i = 0; i < lines.size(); i++) {
+                this._OutputPipe.write(lines.get(i) + "\n");
+                this._OutputPipe.flush();
+            }
+        } catch (Exception ex) {
+            System.out.println("StatusSortFilter: An error occurred writing to the output pipe.");
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    private void closeReader(Reader reader) {
+        try {
+            reader.close();
+        }
+        catch (Exception ex) {
+            System.out.println("StatusSortFilter: An error occurred trying to close a reader.");
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    class StatusComparator implements Comparator<String> {
+        
+        private ArrayList<String> _StatusOrder;
+        
+        public StatusComparator() {
+            this._StatusOrder = new ArrayList<String>();
+            this._StatusOrder.add(0, "ASS");
+            this._StatusOrder.add(1, "NOU");
+            this._StatusOrder.add(2, "RES");
+            this._StatusOrder.add(3, "ROU");
+        }
+        
+        public int compare(String o1, String o2) {
+            
+            String status1 = o1.trim().substring(26, 29);
+            String status2 = o2.trim().substring(26, 29);
+            
+            int pos1 = this._StatusOrder.indexOf(status1);
+            int pos2 = this._StatusOrder.indexOf(status2);
+            
+            int order = -1;
+            if (pos1 == pos2) {
+                order = 0;
+            } else if (pos1 > pos2) {
+                order = 1;
+            }
+            return order;
+        }
+    }
+}
