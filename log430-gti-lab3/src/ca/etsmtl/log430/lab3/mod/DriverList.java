99b5502c7cf4ae4e4b8175b09d4bfd1ca9833d2b
alexandre.fleury.lecompte <alexandre.fleury.lecompte@gmail.com>
2012-07-02 21:18:28 +0000

    Lab3: Applied code automatic code format to the files.

--- log430-gti-lab3/src/ca/etsmtl/log430/lab3/mod/DriverList.java
+++ log430-gti-lab3/src/ca/etsmtl/log430/lab3/mod/DriverList.java
@@ -4,7 +4,7 @@ package ca.etsmtl.log430.lab3.mod;
  * This class is used by various other classes that need to keep a list of
  * drivers on hand. It extends the List class which provides the basic
  * functionality for storage and retrieval of the Driver Object from the list.
- * 
+ *
  * @author A.J. Lattanze, CMU
  * @version 1.5, 2012-Jun-19
  */
@@ -13,133 +13,131 @@ package ca.etsmtl.log430.lab3.mod;
  * Modification Log
  * ***************************************************************************
  * v1.5, R. Champagne, 2012-Jun-19 - Various refactorings for new lab.
- * 
+ *
  * v1.4, R. Champagne, 2012-May-31 - Various refactorings for new lab.
- * 
+ *
  * v1.3, R. Champagne, 2012-Feb-02 - Various refactorings for new lab.
- * 
+ *
  * v1.2, 2011-Feb-02, R. Champagne - Various refactorings, javadoc comments.
- * 
+ *
  * v1.1, 2002-May-21, R. Champagne - Adapted for use at ETS.
- * 
+ *
  * v1.0, 12/29/99, A.J. Lattanze - Original version.
  * **************************************************************************
  */
-
 public class DriverList extends List {
 
-	/**
-	 * Adds a new driver to the list. All the issues of casting are taken care
-	 * of within this class.
-	 * 
-	 * @param driver
-	 */
-	public void addDriver(Driver driver) {
-		appendItemToList((Object) driver);
-	}
+    /**
+     * Adds a new driver to the list. All the issues of casting are taken care
+     * of within this class.
+     *
+     * @param driver
+     */
+    public void addDriver(Driver driver) {
+        appendItemToList((Object) driver);
+    }
 
-	/**
-	 * @return The driver at the current position pointed to by the
-	 *         internal list pointer. Subsequent calls will return the next
-	 *         driver object in the list. A null object is returned if list is
-	 *         empty or the end of list has been reached.
-	 */
-	public Driver getNextDriver() {
-		return (Driver) getItemFromList();
-	}
+    /**
+     * @return The driver at the current position pointed to by the internal
+     * list pointer. Subsequent calls will return the next driver object in the
+     * list. A null object is returned if list is empty or the end of list has
+     * been reached.
+     */
+    public Driver getNextDriver() {
+        return (Driver) getItemFromList();
+    }
 
-	/**
-	 * Determines whether the Driver OBJ is currently in the list.
-	 * 
-	 * @param driver
-	 * @return true if the driver is in the list, false otherwise.
-	 */
-	public boolean findDriver(Driver driver) {
+    /**
+     * Determines whether the Driver OBJ is currently in the list.
+     *
+     * @param driver
+     * @return true if the driver is in the list, false otherwise.
+     */
+    public boolean findDriver(Driver driver) {
 
-		Driver currentObject;
-		boolean done = false;
-		boolean result = false;
+        Driver currentObject;
+        boolean done = false;
+        boolean result = false;
 
-		// Note that this method assumes that all drivers have different
-		// identification numbers.
+        // Note that this method assumes that all drivers have different
+        // identification numbers.
 
-		goToFrontOfList();
+        goToFrontOfList();
 
-		while (!done) {
+        while (!done) {
 
-			currentObject = getNextDriver();
+            currentObject = getNextDriver();
 
-			if (currentObject == null) {
+            if (currentObject == null) {
 
-				done = true;
+                done = true;
 
-			} else {
+            } else {
 
-				if (driver.getDriverID().compareTo(
-						currentObject.getDriverID()) == 0) {
+                if (driver.getDriverID().compareTo(
+                        currentObject.getDriverID()) == 0) {
 
-					result = true;
+                    result = true;
 
-				} // if
+                } // if
 
-			} // if
+            } // if
 
-		} // while
+        } // while
 
-		return (result);
+        return (result);
 
-	} // findDriver
+    } // findDriver
 
-	/**
-	 * Finds a driver in a list using the driverID as the search key.
-	 * 
-	 * @param driverID
-	 * @return if the current list object's driverID matches the driverID
-	 *         passed to the method, the Driver object is returned to the
-	 *         caller. Otherwise, returns null.
-	 */
-	public Driver findDriverByID(String driverID) {
+    /**
+     * Finds a driver in a list using the driverID as the search key.
+     *
+     * @param driverID
+     * @return if the current list object's driverID matches the driverID passed
+     * to the method, the Driver object is returned to the caller. Otherwise,
+     * returns null.
+     */
+    public Driver findDriverByID(String driverID) {
 
-		Driver currentObject = null;
-		boolean done = false;
-		boolean found = false;
+        Driver currentObject = null;
+        boolean done = false;
+        boolean found = false;
 
-		// Note that this method assumes that all drivers have different
-		// identification numbers.
+        // Note that this method assumes that all drivers have different
+        // identification numbers.
 
-		goToFrontOfList();
+        goToFrontOfList();
 
-		while (!done) {
+        while (!done) {
 
-			currentObject = getNextDriver();
+            currentObject = getNextDriver();
 
-			if (currentObject == null) {
+            if (currentObject == null) {
 
-				done = true;
+                done = true;
 
-			} else {
+            } else {
 
-				if (currentObject.getDriverID().compareTo(driverID) == 0) {
+                if (currentObject.getDriverID().compareTo(driverID) == 0) {
 
-					found = true;
-					done = true;
+                    found = true;
+                    done = true;
 
-				} // if
+                } // if
 
-			} // if
+            } // if
 
-		} // while
+        } // while
 
-		if (found) {
+        if (found) {
 
-			return (currentObject);
+            return (currentObject);
 
-		} else {
+        } else {
 
-			return (null);
+            return (null);
 
-		} // if
+        } // if
 
-	} // findDriverByID
-
+    } // findDriverByID
 } // DriverList
